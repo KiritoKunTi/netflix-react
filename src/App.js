74c732b7d2fc01812926a9fa70869456a614414c
duman <duman200354@mail.ru>
2022-10-23 20:25:20 +0600

    auth start

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { AuthContextProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
+import LogIn from './pages/LogIn';
+import SignUp from './pages/LogIn';
+import Account from './pages/LogIn';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={ <Home /> }/>
+          <Route path='/login' element={<LogIn />}/>
+          <Route path='/signup' element={<SignUp />}/>
+          <Route path='/account' element={<Account />}/>
         </Routes>
       </AuthContextProvider>
     </>
